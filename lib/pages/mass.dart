960f8496af898abc55365fee9b8129488d1c0ecb
SamHarv <srharvey93@gmail.com>
2023-03-31 21:41:05 +1100

    Bug fixes

--- lib/pages/mass.dart
+++ lib/pages/mass.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '/constants.dart';
 import '/widgets/button.dart';
@@ -19,6 +20,8 @@ class _MassPageState extends ConsumerState<MassPage> {
   final baselineMassController = TextEditingController();
   double? dailyMass;
   double? setBaselineMass;
+  DateTime? date;
+  String? formatedDate;
 
   @override
   Widget build(BuildContext context) {
@@ -26,16 +29,19 @@ class _MassPageState extends ConsumerState<MassPage> {
     final double mediaHeight = MediaQuery.of(context).size.height;
     dailyMass = ref.watch(bodyMass);
     setBaselineMass = ref.watch(baselineMass);
+    date = ref.watch(dateSelect);
+    formatedDate = DateFormat.yMMMMd().format(date!);
 
     return Scaffold(
       appBar: const NestedAppBar(),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             const Text('Select Date for Entry'),
             Button(
-              pressed: () {
-                showDatePicker(
+              pressed: () async {
+                final selectedDate = await showDatePicker(
                     context: context,
                     initialDate: DateTime.now(),
                     firstDate: DateTime(2000),
@@ -60,20 +66,26 @@ class _MassPageState extends ConsumerState<MassPage> {
                         child: child!,
                       );
                     });
+                if (selectedDate == null) {
+                  return;
+                } else {
+                  ref.read(dateSelect.notifier).state = selectedDate;
+                }
               },
               height: 0.09,
               width: mediaWidth <= 750 ? 0.35 : 0.2,
-              colour: primaryColour,
+              colour: thirdColour,
               child: const Center(
                 child: Text(
                   'Select Date',
                   style: TextStyle(
                     fontSize: 20,
-                    color: thirdColour,
+                    color: secondaryColour,
                   ),
                 ),
               ),
             ),
+            Text(formatedDate!),
             const Text('Record your daily body mass below:'),
             MyTextField(
               mediaWidth: mediaWidth,
@@ -96,18 +108,21 @@ class _MassPageState extends ConsumerState<MassPage> {
             ),
             Button(
               pressed: () {
-                ref.read(bodyMass.notifier).state =
-                    double.parse(massController.text);
+                // ignore: unnecessary_null_comparison
+                massController.text == ''
+                    ? ref.read(bodyMass.notifier).state = ref.watch(bodyMass)
+                    : ref.read(bodyMass.notifier).state =
+                        double.parse(massController.text);
                 dailyMass = ref.watch(bodyMass);
                 // ignore: unnecessary_null_comparison
-                ref.read(baselineMass.notifier).state == null
+                baselineMassController.text == ''
                     ? ref.read(baselineMass.notifier).state =
-                        double.parse(baselineMassController.text)
+                        ref.watch(baselineMass)
                     : ref.read(baselineMass.notifier).state =
-                        ref.watch(baselineMass);
+                        double.parse(baselineMassController.text);
                 setBaselineMass = ref.watch(baselineMass);
                 //Beamer.of(context).beamBack();
-                //Add dailyMass result to list of Masses or Map with Date : Mass
+                //Add dailyMass result to list of Masses or Map with Date : Mass?
                 //show snackBar
               },
               height: 0.09,
--- lib/providers.dart
+++ lib/providers.dart
@@ -2,3 +2,4 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final bodyMass = StateProvider<double>((ref) => 114.5);
 final baselineMass = StateProvider<double>((ref) => 115.0);
+final dateSelect = StateProvider<DateTime>((ref) => DateTime.now());
